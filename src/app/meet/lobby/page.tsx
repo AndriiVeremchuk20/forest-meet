adfe6c76df22f5f499c0699181be0d5a60071405
Andrii <andrejveremcuk712@gmail.com>
2024-01-15 12:30:36 +0200

    feat: made simply create room process

--- src/app/meet/lobby/page.tsx
+++ src/app/meet/lobby/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import useUserStore from "@/store";
 import { api } from "@/trpc/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const LobbyPage = () => {
-  const userStore = useUserStore();
   const router = useRouter();
   const session = useSession();
 
   const createRoomMutation = api.agora.createRoom.useMutation({
     onSuccess: async (data) => {
       console.log(data);
-      userStore.setUser(data);
       router.push(`/meet/room?id=${data.channelName}`);
     },
     onError(error) {
@@ -25,24 +22,11 @@ const LobbyPage = () => {
     createRoomMutation.mutate();
   };
 
-  const joinToRoomMutation = api.agora.joinToRoom.useMutation({
-    onSuccess: async (data) => {
-      console.log(data);
-      userStore.setUser(data);
-      router.push(`/meet/room?id=${data.channelName}`);
-    },
-    onError(error) {
-      console.log(error.message);
-    },
-  });
-
   const onJoinClick = () => {
     const channelName = prompt("Enter a channel name");
 
     if (channelName !== null && channelName.trim() !== "") {
-      joinToRoomMutation.mutate({ channelName: channelName } as {
-        channelName: string;
-      });
+      router.push(`/meet/room?id=${channelName}`);
     }
   };
 
--- src/server/api/routers/agora.ts
+++ src/server/api/routers/agora.ts
@@ -1,29 +1,15 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { TRPCError } from "@trpc/server";
 import AgoraServices from "@/utils/agora";
 
 export const agoraRouter = createTRPCRouter({
   createRoom: publicProcedure.mutation(async () => {
+    // just generate a channel name for future room
     const channelName = await AgoraServices.generateChannelName();
-    const uid = AgoraServices.generateUid();
-    const expireTime = 3600; // 1 hour
-
-    const token = AgoraServices.generateRtcToken({
-      uid,
-      channelName,
-      expireTime,
-    });
-
-    if (!token) {
-      throw new TRPCError({ message: "invalid token", code: "BAD_REQUEST" });
-    }
 
     return {
       channelName,
-      uid,
-      token,
     };
   }),
   joinToRoom: publicProcedure
