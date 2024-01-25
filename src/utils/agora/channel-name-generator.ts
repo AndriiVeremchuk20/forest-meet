fbdef18fd87beb320f2fe57af0d03bee2dc4fcb0
Andrii <andrejveremcuk712@gmail.com>
2024-01-25 19:01:04 +0200

    ref: update channel name generator

--- src/utils/agora/channel-name-generator.ts
+++ src/utils/agora/channel-name-generator.ts
@@ -1,35 +1,8 @@
-// function to generate random channel name
-
-import ky from "ky";
-import {generate} from "random-words";
-
-const RANDOM_WORD_API_URL = "https://random-word-api.herokuapp.com";
-
-const randomWordApiClient = ky.create({
-  prefixUrl: RANDOM_WORD_API_URL,
-  headers: {
-    accept: "application/ld+json",
-  },
-});
-
-const getRandomWord = async () => {
-  try {
-    const randomWord = await randomWordApiClient.get("word").json<[string]>();
-
-    return randomWord[0];
-  } catch (error) {
-    console.log(error);
-  }
-};
+import { generate } from "random-words";
 
 const generateChannelName = async () => {
   const words = generate(5) as string[];
-	//console.log(generate().join());
-	//const words = await Promise.all([
-    //getRandomWord(),
-    //getRandomWord(),
-    //getRandomWord(),
-  //]);
+
   return words.join("-");
 };
 
